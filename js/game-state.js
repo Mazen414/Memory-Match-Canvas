bf6c3de8cb88e9f448d71e8e1447c73d2d741ead
Mazen414 <mazenmohamed22@istanbularel.edu.tr>
2026-06-30 18:16:21 +0300

    Feat: support level parameters mapping for progressive scaling matrix

--- js/game-state.js
+++ js/game-state.js
@@ -16,3 +16,26 @@ function startGame() {
   loadLevel();
   gameState = 'PLAYING';
 }
+
+// Level Geometry Vector Builder Engine
+function loadLevel() {
+  clearInterval(timer);
+
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
+  matchedPairs = 0;
+  extraTimeUsed = false;
+
+  // Multi-tier structural parameters scaling matrix [cite: 44, 47]
+  if (level === 1) { totalPairs = 4; timeLeft = 60; }
+  else if (level === 2) { totalPairs = 6; timeLeft = 53; }
+  else if (level === 3) { totalPairs = 8; timeLeft = 46; }
+  else if (level === 4) { totalPairs = 8; timeLeft = 39; }
+  else if (level === 5) { totalPairs = 8; timeLeft = 32; }
+  else if (level === 6) { totalPairs = 8; timeLeft = 25; }
+
+  gameMessage = "";
+  createBoard();
+  startTimer();
+}
