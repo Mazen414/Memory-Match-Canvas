975a23bd58ef1283b27f51456c3a40593638cc6c
Mazen414 <mazenmohamed22@istanbularel.edu.tr>
2026-06-30 17:11:48 +0300

    Feat: add array-randomizer module utilizing Fisher-Yates algorithm

---
+++ js/shuffle-engine.js
@@ -0,0 +1,8 @@
+// Strict In-Place Fisher-Yates Card Array Array Randomizer
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
